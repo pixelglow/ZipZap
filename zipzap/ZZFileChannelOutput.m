1cae91d1e5b2de4ae85c76521b615a3223181b8e
Glen Low <glen.low@pixelglow.com>
2013-07-30 13:54:09 +0800

    Fix writing entries > 2G erroring with POSIX error EINVAL (fix #39)

--- zipzap/ZZFileChannelOutput.m
+++ zipzap/ZZFileChannelOutput.m
@@ -46,15 +46,23 @@
 - (BOOL)writeData:(NSData*)data
 			error:(NSError**)error
 {
-	if (write(_fileDescriptor, data.bytes, data.length) == -1)
+	// output up to INT_MAX bytes at a time: Darwin errors with EINVAL if we write > INT_MAX bytes
+	const uint8_t* bytes;
+	NSInteger bytesLeft;
+	NSInteger bytesWritten;
+	for (bytes = (const uint8_t*)data.bytes, bytesLeft = data.length;
+		 bytesLeft > 0;
+		 bytes += bytesWritten, bytesLeft -= bytesWritten)
 	{
-		if (error)
-			*error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:nil];
-		return NO;
+		bytesWritten = write(_fileDescriptor, bytes, MIN(bytesLeft, INT_MAX));
+		if (bytesWritten == -1)
+		{
+			if (error)
+				*error = [NSError errorWithDomain:NSPOSIXErrorDomain code:errno userInfo:nil];
+			return NO;
+		}
 	}
-	else
-		return YES;
-		
+	return YES;
 }
 
 - (BOOL)truncateAtOffset:(uint32_t)offset
