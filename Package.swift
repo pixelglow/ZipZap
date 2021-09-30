27f83270beb391816b3e55a84683a87e1a048896
hcszt <76826622+hcszt@users.noreply.github.com>
2021-09-30 19:11:37 +0200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ZipZap",
+    products: [
+        .library(
+            name: "ZipZap",
+            targets: ["ZipZap"])
+    ],
+    targets: [
+        .target(
+            name: "ZipZap",
+            path: "ZipZap"
+        )
+    ]
+)
